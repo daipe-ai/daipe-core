c9c38c281dc1cf24c2cac7935597efd9e53511ac
Lukas Langr <lukas.langr@datasentics.com>
2021-09-23 16:25:13 +0200

    Add daipecore.imports

---
+++ src/daipecore/imports.py
@@ -0,0 +1,3 @@
+from daipecore.decorator.notebook_function import notebook_function  # noqa: F401
+from daipecore.widgets.Widgets import Widgets  # noqa: F401
+from daipecore.widgets.get_widget_value import get_widget_value  # noqa: F401
